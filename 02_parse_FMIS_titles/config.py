3ff9f7cfa88499ea66265dc2ac612a4d52ae4983
Hannah Lu <hannah.lu@yale.edu>
2026-08-11 11:21:55 -0400

    update config for validation FMIS GIS gemini run

--- 02_parse_FMIS_titles/config.py
+++ 02_parse_FMIS_titles/config.py
@@ -44,17 +44,17 @@ GEMINI_MODEL_ID = "gemini-2.5-flash"
 # GEMINI_MODEL_ID = "gemini-3.5-flash"
 
 # SET GEMINI PROMPT 
-prompt_version = 5
+prompt_version = 6
 
 # SET FILE IDENTIFIERS
-RUN_PREFIX = f"interstate_newconstr_1yrPR511_ct_v{prompt_version}"
+RUN_PREFIX = f"VAL_fmis_gis_5k_v{prompt_version}"
 
 # SET RESUME PARAMETERS
 REUSE_OLD_RESULTS = False
 RESUME_RUN_IDENTIFIER = None
 
 # SET FILE PATHS 
-INPUT_PATH = INTERMEDIATE_DATA_DIR / "geocode_eval" / "splits" / "pr511_cty_val_fmis_interstate_newconstr_project_titles.dta"
+INPUT_PATH = INTERMEDIATE_DATA_DIR / "geocode_eval" / "splits" / "VAL_fmis_gis_project_titles.dta"
 
 # SET SCHEMA
 page_schema = Project
@@ -62,13 +62,9 @@ page_schema = Project
 # SET SAMPLING PARAMETERS
 ROW_INDICES = None 
 # ROW_INDICES = [27, 28, 29, 30]          # manual override; if set, skip auto sample
-# test on subset of projects coded as "other_landmark" from the 1000-project sample 
-# ROW_INDICES = [4764,
-# 9673, 17318, 9690, 19604, 10831, 17351, 18157, 10842, 15966, 15541, 13380, 1878, 1859, 15059, 19036, 4029, 5523, 8094, 9179, 8081, 11194, 9229, 5718, 15752, 9216, 14261, 9247, 16819, 5934, 12426, 1528, 20162, 19362, 18684, 10650, 14922]
-# ROW_INDICES = [i - 1 for i in ROW_INDICES]
-SAMPLE_N = None                # if set (and ROW_INDICES is None), sample this many rows
-# SAMPLE_STRATIFY_BY = ["state_fips", "post_1970_auth", "below_median_cost"]
+SAMPLE_N = 5000                # if set (and ROW_INDICES is None), sample this many rows
 # SAMPLE_N = None # use full dataset
+# SAMPLE_STRATIFY_BY = ["state_fips", "post_1970_auth", "below_median_cost"]
 RANDOM_SEED = 42
 
 # ------------------------------------------------------------------------------
