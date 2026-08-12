ccc85301ea61bf5271bc16f9163aa016cb54a828
Hannah Lu <hannah.lu@yale.edu>
2026-08-12 12:30:37 -0400

    sync slurm script to server

--- 00_scripts/parse_titles_day.sh
+++ 00_scripts/parse_titles_day.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=gemini_parse_titles_day
 #SBATCH --output="/home/hl2266/project_pi_zdl3/shared/FMIS project/slurm_logs/gemini_parse_titles_day_%j.log"
 #
-#SBATCH --time=10:00:00
+#SBATCH --time=20:00:00
 #SBATCH --mem=1G
 #SBATCH --ntasks=1
 #
